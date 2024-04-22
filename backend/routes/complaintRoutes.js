b63b70619acfa8f1f7ba4223d2c2372d3dd55220
Code-GPT1 <aasonawane_b21@et.vjti.ac.in>
2024-04-22 23:07:13 +0530

    post route of student

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {Router} from "express";
+import upload from '../multerConfig.js';
 const complaintRoutes=Router();
-import { getStudentComplaint,deleteStudentComplaint } from "../controller/complaintController.js";
+import { getStudentComplaint,deleteStudentComplaint,postStudentComplaint } from "../controller/complaintController.js";
 
 complaintRoutes.route("/complaint/hostel/student").get(getStudentComplaint);
 complaintRoutes.route("/complaint/hostel/student/:id").delete(deleteStudentComplaint);
+complaintRoutes.route("/complaint/hostel/student").post(upload.single('image'),postStudentComplaint);
 export default complaintRoutes;
